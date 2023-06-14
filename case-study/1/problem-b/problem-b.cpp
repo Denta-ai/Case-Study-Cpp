aab4e1ad35e06e57f0dcb00258c51691747ba612
Denta-ai <septyandenta@gmail.com>
2023-06-14 22:06:42 +0700

    1st optimization and adding comment on problem-b.cpp

--- case-study/1/problem-b/problem-b.cpp
+++ case-study/1/problem-b/problem-b.cpp
@@ -4,27 +4,86 @@
 class ChickenShopReceipts {
   public:
     ChickenShopReceipts(const double friedChickenPrice, const double grilledChickenPrice, const double discountCost, const double purchaseMinim);
-    void acceptUserInput();
-    void calculateTotalPrice();
-    void printReceipts();
+    void acceptUserInput(); // Accepts user input for order details
+    void calculateTotalPrice(); // Calculates the total price including discounts
+    void printReceipts(); // Prints the receipt
 
   private:
     double priceFriedChicken;
     double priceGrilledChicken;
     double totalPrice;
-    double Discount;
+    double discount;
     double priceDiscount;
     double minimPurchase;
     std::string customerName;
-    int selectedOrder;
+    int selectedMenuOption;
     int totalAmount;
-    
 };
 
 ChickenShopReceipts::ChickenShopReceipts(const double friedChickenPrice, const double grilledChickenPrice, const double discountCost, const double purchaseMinim)
-  : priceFriedChicken(friedChickenPrice), priceGrilledChicken(grilledChickenPrice), Discount(discountCost), minimPurchase(purchaseMinim) {};
+    : priceFriedChicken(friedChickenPrice), priceGrilledChicken(grilledChickenPrice), discount(discountCost), minimPurchase(purchaseMinim) {}
 
-int main(){
+void ChickenShopReceipts::acceptUserInput() {
+  std::cout << "Input your order" << std::endl;
+  std::cout << "1. Fried Chicken\t: IDR 17,000" << std::endl;
+  std::cout << "2. Grilled Chicken\t: IDR 21,000" << std::endl;
+  std::cout << "Discount 10%, if the purchase exceeds IDR 45,000 " << std::endl;
+  std::cout << "Name\t\t: ";
+  std::cin >> customerName;
+  std::cout << "Input menu\t: ";
+  std::cin >> selectedMenuOption;
+  std::cout << std::endl;
+
+  if (selectedMenuOption != 1 && selectedMenuOption != 2) {
+      std::cout << "Error: Invalid menu option!" << std::endl;
+      exit(0);
+  }
+
+  // Determine the menu name and price based on the selected option
+  std::string menuName = (selectedMenuOption == 1) ? "Fried Chicken" : "Grilled Chicken";
+  double menuPrice = (selectedMenuOption == 1) ? priceFriedChicken : priceGrilledChicken;
+
+  std::cout << std::endl;
+  std::cout << "Menu chosen: " << menuName << std::endl;
+  std::cout << menuName << "\t: IDR " << menuPrice << std::endl;
+
+  std::cout << "Input amount\t: ";
+  std::cin >> totalAmount;
+
+  if (totalAmount <= 0) {
+      std::cout << "Error: Invalid amount!" << std::endl;
+      exit(0);
+  }
+}
+
+void ChickenShopReceipts::calculateTotalPrice() {
+  // Determine the menu price based on the selected option
+  double menuPrice = (selectedMenuOption == 1) ? priceFriedChicken : priceGrilledChicken;
+  totalPrice = menuPrice * totalAmount;
+
+  if (totalPrice > minimPurchase) {
+      priceDiscount = totalPrice * discount;
+      totalPrice -= priceDiscount;
+  }
+}
+
+void ChickenShopReceipts::printReceipts() {
+  std::cout << std::endl << std::endl;
+  std::cout << "Receipt" << std::endl;
+  std::cout << "Menu chosen" << std::endl;
+  std::cout << "Customer\t\t: " << customerName << std::endl;
+
+  // Determine the menu name and price based on the selected option
+  std::string menuName = (selectedMenuOption == 1) ? "Fried Chicken" : "Grilled Chicken";
+  double menuPrice = (selectedMenuOption == 1) ? priceFriedChicken : priceGrilledChicken;
+
+  std::cout << selectedMenuOption << ". " << menuName << "\t: IDR " << menuPrice << std::endl;
+  std::cout << "Total amount\t\t: " << totalAmount << std::endl;
+  std::cout << "Discount 10%, if the purchase exceeds IDR 45,000 " << std::endl;
+  std::cout << "Total price\t\t: IDR " << totalPrice << std::endl;
+}
+
+int main() {
   const double friedChickenPrice = 17000;
   const double grilledChickenPrice = 21000;
   const double discountCost = 0.10;
@@ -37,69 +96,3 @@ int main(){
 
   return 0;
 }
-
-void ChickenShopReceipts::acceptUserInput(){
-  std::cout << "Input your order" << std::endl;
-  std::cout << "1. Fried Chicken\t: IDR 17,000" << std::endl;
-  std::cout << "2. Grilled Chicken\t: IDR 21,000" << std::endl;
-  std::cout << "Discount 10%, if the purchase exceeds IDR 45,000 " << std::endl;
-  std::cout << "Name\t\t: "; std::cin >> customerName;
-  std::cout << "Input menu\t: "; std::cin >> selectedOrder;
-  std::system("cls");
-
-  if (selectedOrder == 1){
-    std::cout << "Menu chose 1" << std::endl;
-    std::cout << "Fried chicken\t: IDR 17,000" << std::endl;
-    std::cout << "Input amount\t: "; std::cin >> totalAmount;
-    calculateTotalPrice();
-  }
-  if (selectedOrder == 2){
-    std::cout << "Menu chose 2" << std::endl;
-    std::cout << "Grilled chicken\t: IDR 21,000" << std::endl;
-    std::cout << "Input amount\t: "; std::cin >> totalAmount;
-    calculateTotalPrice();
-  }
-  if (selectedOrder != 1 && selectedOrder != 2){
-    std::system("cls");
-    std::cout << "Error, wrong input!";
-    exit(0);
-  }
-
-}
-
-void ChickenShopReceipts::calculateTotalPrice(){
-  std::system("cls");
-  if (selectedOrder == 1){
-    totalPrice = priceFriedChicken*totalAmount;
-  }
-  if (selectedOrder == 2){
-    totalPrice = priceGrilledChicken*totalAmount;
-  }
-  if (totalPrice > minimPurchase){
-    priceDiscount = totalPrice*Discount;
-    totalPrice -= priceDiscount;
-  }
-}
-
-void ChickenShopReceipts::printReceipts(){
-  std::cout << "Receipt" << std::endl;
-  std::cout << "Menu chose" << std::endl;
-  std::cout << "Customer\t\t: " << customerName << std::endl;
-
-  if (selectedOrder == 1){
-    std::cout << "1. Fried Chicken\t: IDR 17,000" << std::endl;
-    std::cout << "total amount\t\t: " << totalAmount << std::endl;
-    std::cout << "Discount 10%, if the purchase exceeds IDR 45,000 " << std::endl;
-    std::cout << "Total price\t\t: " << totalPrice << std::endl;
-
-  }
-  if (selectedOrder == 2){
-    std::cout << "2. Grilled Chicken\t: IDR 21,000" << std::endl;
-    std::cout << "total amount\t\t: " << totalAmount << std::endl;
-    std::cout << "Discount 10%, if the purchase exceeds IDR 45,000 " << std::endl;
-    std::cout << "Total price\t\t: " << totalPrice << std::endl;
-
-  }
-
-}
-
