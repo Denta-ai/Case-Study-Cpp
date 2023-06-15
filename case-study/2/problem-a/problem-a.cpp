b72499baf5a7e70f7f16833043b5add594f0f85e
Denta-ai <septyandenta@gmail.com>
2023-06-15 21:41:18 +0700

    solving problem-a 1.0

--- case-study/2/problem-a/problem-a.cpp
+++ case-study/2/problem-a/problem-a.cpp
@@ -7,7 +7,7 @@
 //   5. Grilled chicken: IDR 25000
 
 //   The restaurant provides delivery order services, for customers whose homes are less than
-//   3KM will be given a postage fee of 15,000, if it is more than that, a postage fee of 15,000 will be charged
+//   3KM will be given a postage fee of 15,000, if it is more than that, a postage fee will be charged
 //   25000. If the total purchase is more than 25000, you will receive a discount on shipping costs of 3000.
 //   If the total purchase exceeds 50000, a discount of shipping 5000 dan will be given
 //   discount as much as 15%. And if the total purchase exceeds 150000, a discount will be given
@@ -19,54 +19,82 @@
 #include <iostream>
 #include <string>
 #include <array>
+#include <vector>
 
 class RestaurantReceipts {
   public:
     RestaurantReceipts();
     void acceptInputUser();
-    void calculateProcess();
-    void printReceipts();
+    void calculatePrice();
+    void customerDistance();
+    void priceDiscount();
   private:
     std::array<std::string, 5> menu;
     std::array<double, 5> menuPrice;
-    std::array<double, 3> priceForDiscount;
-    std::array<double, 2> discountPrice;
-    std::array<double, 3> discountShipping;
-    const double lowPostageCost;
-    const double highPostageCost;
+    int amount;
+    int selectedMenu;
     double totalPrice;
-    int selectedMenuChoice;
-    int userDistance;
+    int distance;
 };
 
 RestaurantReceipts::RestaurantReceipts() 
   : menu{"1.Greprek Chicken\t IDR 21000", "2.Fried chicken\t\t IDR 17000", 
           "3.Fried shrimp\t\t IDR 19000", "4.Fried squid\t\t IDR 20000", "5.Grilled chicken\t IDR 25000"},
-    menuPrice{21000, 17000, 19000, 20000, 25000},
-    priceForDiscount{25000, 50000, 150000},
-    discountPrice{0.15, 0.35},
-    discountShipping{3000, 5000, 8000},
-    lowPostageCost(15000), highPostageCost(25000){}
+    menuPrice{21000, 17000, 19000, 20000, 25000} {}
 
 void RestaurantReceipts::acceptInputUser(){
   std::cout << "Menu:" << std::endl;
   for (int i = 0; i < size(menu); i++){
     std::cout << menu[i] << std::endl;
   }
-  std::cout << "Input choice: "; std::cin >> selectedMenuChoice;
-  std::cout << "Input your distance: "; std::cin >> userDistance;
-}
-
-void RestaurantReceipts::calculateProcess(){
+  std::cout << "Input your choice: "; std::cin >> selectedMenu;
+  std::cout << "Input how many order: "; std::cin >> amount;
   
 }
 
-void RestaurantReceipts::printReceipts(){
-  
+void RestaurantReceipts::calculatePrice(){
+  totalPrice = menuPrice[selectedMenu-1] * amount;
+  std::cout << "Your order: " << std::endl;
+  std::cout << menu[selectedMenu] << std::endl;
+  std::cout << "Total price: " << totalPrice << std::endl;
+}
+
+void RestaurantReceipts::priceDiscount(){
+  if (totalPrice > 25000 && totalPrice <= 50000){
+    totalPrice -= 3000;
+  }
+  if (totalPrice > 50000 && totalPrice <= 150000){
+    int temp = totalPrice;
+    temp *= 0.15;
+    totalPrice -= temp;
+    totalPrice -= 5000;
+  }
+  if (totalPrice > 150000){
+    int temp = totalPrice;
+    temp *= 0.35;
+    totalPrice -= temp;
+    totalPrice -= 8000;
+  }
+}
+
+void RestaurantReceipts::customerDistance(){
+  if (distance <= 3){
+    totalPrice += 15000;
+  }else {
+    totalPrice += 25000;
+  }
 }
 
 int main(){
+  char repeatOrder = 'y';
   RestaurantReceipts receipts;
-  receipts.acceptInputUser();
+  while(repeatOrder == 'y'){
+    receipts.acceptInputUser();
+    receipts.calculatePrice();
+    std::cout << "You want to order again? (y/n): "; std::cin >> repeatOrder;
+    if (repeatOrder == 'n'){
+      break;
+    }
+  }
   return 0;
 }
