14a59fc98f87dbdf67edf82742e75d2c48310fe9
Denta-ai <septyandenta@gmail.com>
2023-06-28 21:38:59 +0700

    update solved.cpp on expenses-tracker folder

--- case-study/Expense-Tracker-and-Savings-Calculator/solved.cpp
+++ case-study/Expense-Tracker-and-Savings-Calculator/solved.cpp
@@ -1,64 +1,52 @@
+
+// Andi mendapatkan uang saku dengan jumlah sama setiap bulannya. Ia gunakan uang saku itu untuk 
+//   membeli beberapa keperluan kuliah dan jajannya. Akan tetapi, ia juga ingin menabung dari sisa uang 
+//   saku yang ia miliki, walaupun tidak tentu setiap bulannya. 
+//   Andi meminta kawannya yang kuliah Informatika untuk membuatkan aplikasi untuk menghitung itu 
+//   semua dengan menampilkan pengeluaran tiap bulan, total pengeluaran, dan uang tabungannya saat ini.
+//   Buatlah program dengan tim anda, dengan menerapkan materi iteratif-rekursif yang baru saja 
+//   dipelajari dimateri praktikum minggu ini.
+// Catatan :
+// - Menampilkan data secara Iteratif
+// - Menggunakan Class
+
 #include <iostream>
 #include <vector>
 
-class PocketMoneyCalculator {
-private:
-    std::vector<double> expenses;
+class moneyCalculator {
+    public:
+        void income();
+        void expenses();
+        void saving();
+    private:
 
-public:
-    void addExpense(double amount) {
-        expenses.push_back(amount);
-    }
-
-    double calculateTotalExpenses() {
-        double total = 0.0;
-        for (double expense : expenses) {
-            total += expense;
-        }
-        return total;
-    }
-
-    double calculateCurrentSavings(double pocketMoney) {
-        double totalExpenses = calculateTotalExpenses();
-        return pocketMoney - totalExpenses;
-    }
-
-    void displayMonthlyExpenses() {
-        int month = 1;
-        for (double expense : expenses) {
-            std::cout << "Month " << month << " expense: $" << expense << std::endl;
-            month++;
-        }
-    }
 };
 
-int main() {
-    double pocketMoney;
-    int numOfMonths;
+void moneyCalculator::income(){
+    int moneyPerMonth = 0;
+    char spentMoney;
+    int items = 0;
+    std::string itemsName;
+    int itemsPrice;
 
-    std::cout << "Enter the amount of pocket money Andi receives every month: $";
-    std::cin >> pocketMoney;
-
-    std::cout << "Enter the number of months: ";
-    std::cin >> numOfMonths;
-
-    PocketMoneyCalculator calculator;
-
-    for (int i = 0; i < numOfMonths; i++) {
-        double expense;
-        std::cout << "Enter the expense for month " << (i + 1) << ": $";
-        std::cin >> expense;
-        calculator.addExpense(expense);
+    std::cout << "Input your money income per months : "; std::cin >> moneyPerMonth;
+    std::cout << "Your stable income money : " << moneyPerMonth << std::endl;
+    std::cout << "Did you spend your money? : "; std::cin >> spentMoney;
+    if (spentMoney=='y'){
+        while (spentMoney=='y'){
+            items++;
+            std::cout << "Your expenses" << std::endl;
+            std::cout << "Item " << items << " : "; std::cin >> itemsName;
+            std::cout << "Price " << items << " : "; std::cin >> itemsPrice;
+            std::cout << "Again ? : "; std::cin >> spentMoney;
+            if (spentMoney=='n'){
+                exit(0);
+            }
+        }
     }
+}
 
-    std::cout << std::endl;
-    calculator.displayMonthlyExpenses();
-
-    double totalExpenses = calculator.calculateTotalExpenses();
-    double currentSavings = calculator.calculateCurrentSavings(pocketMoney);
-
-    std::cout << "Total expenses: $" << totalExpenses << std::endl;
-    std::cout << "Current savings: $" << currentSavings << std::endl;
-
-    return 0;
+int main(){
+    moneyCalculator count;
+    count.income();
 }
