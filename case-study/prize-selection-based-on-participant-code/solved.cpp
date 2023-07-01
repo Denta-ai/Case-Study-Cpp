4c8544529ab66c0ab2b6bc013fab9fb4066cf54f
Denta-ai <septyandenta@gmail.com>
2023-07-01 21:58:24 +0700

    update 2 solved.cpp on folder prize-selection...

--- case-study/prize-selection-based-on-participant-code/solved.cpp
+++ case-study/prize-selection-based-on-participant-code/solved.cpp
@@ -4,20 +4,49 @@ using namespace std;
 
 int getStringLength(string strings){
   int length = 0;
-  while(strings[length] != '\0'){
-    length+=1;
+  while (strings[length] != '\0'){
+    length += 1;
   }
   return length;
 }
 
-void findSubString(string code){
+string delString(string lastSlice){
+  int lastSliceLength = getStringLength(lastSlice);
+  string deletedString = "";
+  for (int character = 1; character < lastSliceLength; character++){
+    deletedString += lastSlice[character];
+  }
+  return deletedString;
+}
+
+void findSubString(string code, string key){
   int codeLength = getStringLength(code);
+  int keyLength = getStringLength(key);
+  string slices[codeLength];
+  string lastSlice = "";
+
+  for (int i = 0; i < codeLength; i++){
+    lastSlice += code[i];
+    int lastSliceLength = getStringLength(lastSlice);
+
+    if (lastSliceLength == keyLength && lastSlice != key){
+      slices[i] = lastSlice;
+      lastSlice = delString(lastSlice);
+    }
+    if (lastSliceLength == keyLength && lastSlice == key){
+      int startIndex = (i - keyLength) + 1;
+      int lastIndex = i;
+      cout << key << " " << "ditemukan pada indeks " << startIndex << " - " << lastIndex << " dari string " << code;
+      break;
+    }
+  }
 }
 
 int main(){
   string code;
+  string key = "70";
   cout << "Input: "; cin >> code;
-  findSubString(code);
+  findSubString(code, key);
 
   return 0;
 }
